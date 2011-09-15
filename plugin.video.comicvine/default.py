c2f8ff461b077a25f5304c730db0064dbdb64cba
dan653 <inbox.dan@gmail.com>
2011-09-15 23:38:48 +0200

    Fan art set for all items & categories

--- plugin.video.comicvine/default.py
+++ plugin.video.comicvine/default.py
@@ -125,6 +125,7 @@ def addLink(name, url, iconimage):
     ok=True
     liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
     liz.setInfo( type="Video", infoLabels={ "Title": name } )
+    liz.setProperty("fanart_image", "special://home/addons/plugin.video.comicvine/fanart.jpg")
     ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
     return ok
 
@@ -132,6 +133,7 @@ def addDir(name, url, mode, iconimage):
     u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
     ok=True
     liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+    liz.setProperty("fanart_image", "special://home/addons/plugin.video.comicvine/fanart.jpg")
     liz.setInfo( type="Video", infoLabels={ "Title": name } )
     ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
     return ok
